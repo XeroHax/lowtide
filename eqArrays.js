084651b291a1b6f18f1e05119b9cd8d93cbe062f
Roland Fisher <roland.p.fisher@gmail.com>
2023-04-22 19:39:16 +0000

    Added eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,16 @@
+const assertEqual = function (actual, expected) {
+  actual === expected
+    ? console.log(`😃 Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`😢 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = function (arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2]), true);
