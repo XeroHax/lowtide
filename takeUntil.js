5474ecd0561b0c14e0d004a8a8d3eb3f65d4e93a
Roland Fisher <roland.p.fisher@gmail.com>
2023-04-30 20:43:11 +0000

    added takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,43 @@
+const eqArrays = function (arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  eqArrays(arr1, arr2)
+    ? console.log("😃 Assertion Passed: the arrays are equal.")
+    : console.log("😢 Assertion Failed: the arrays are not the same.");
+};
+
+const takeUntil = function (array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      return array.slice(0, i);
+    }
+  }
+  return array;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, (x) => x < 0);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+console.log("---");
+
+const data2 = [
+  "I've",
+  "been",
+  "to",
+  "Hollywood",
+  ",",
+  "I've",
+  "been",
+  "to",
+  "Redwood",
+];
+const results2 = takeUntil(data2, (x) => x === ",");
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
